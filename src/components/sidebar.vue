d0bd694349e0f24513554b4e19ff7f698a536890
edson2001 <djiedson413@gmial.com>
2021-07-08 10:04:25 +0100

    estrututa completa html

---
+++ src/components/sidebar.vue
@@ -0,0 +1,43 @@
+<template>
+    
+    <div class="user">
+
+        <img src="https://www.pavilionweb.com/wp-content/uploads/2017/03/man-300x300.png" alt="">
+        <h3>Edson Dos Santos Bandola</h3>
+
+        <ul class="user-options">
+            <li>
+                <a href="">Home</a>
+            </li>
+            <li>
+                <a href="">Messages</a>
+            </li>
+            <li>
+                <a href="">Group Messages</a>
+            </li>
+            <li>
+                <a href="">Favorites Contacts</a>
+            </li>
+        </ul>
+
+        <ul class="settings-options">
+            <li>
+                <a href="">Settings</a>
+            </li>
+            <li>
+                <a href="">Profile</a>
+            </li>
+            <li>
+                <a href="">Logout</a>
+            </li>
+        </ul>
+
+    </div>
+
+</template>
+
+<script>
+export default {
+    
+}
+</script>
