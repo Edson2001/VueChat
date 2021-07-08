9356ab0dae4f451ba404f2ac166ad2ddff99289a
edson2001 <djiedson413@gmial.com>
2021-07-08 10:55:57 +0100

    terminando a sidebarChat

--- src/components/sidebarchat.vue
+++ src/components/sidebarchat.vue
@@ -1,7 +1,34 @@
 <template>
     
    <div class="sidebarChat">
-       <p>mldffdfffd</p>
+       
+    <div class="options">
+        <span>Chats</span>
+        <span>
+            <i class="fas fa-sort-amount-down"></i>
+        </span>
+    </div>
+
+    <div class="search-options">
+        <input type="text">
+        <i class="fa fa-search"></i>
+    </div>
+
+    <ul class="list-user">
+        <li>
+            <a href="">
+                <img src="https://franklininfo.net/images/icons/man-beard.png" alt="">
+                <div class="user-details">
+                    <h6 class="user-name">Júnior Andrade</h6>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                </div>
+                <div class="list-data">
+                    <span>22:50 PM</span>
+                </div>
+            </a>
+        </li>
+    </ul>
+
    </div>
 
 </template>
